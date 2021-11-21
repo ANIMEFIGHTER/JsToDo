6ceec66d80af0c095b6c2365aa95ebbc90e4b646
ANIMEFIGHTER <56396779+ANIMEFIGHTER@users.noreply.github.com>
2021-11-21 19:51:06 +0300

    Add files via upload

---
+++ ToDo-script.js
@@ -0,0 +1,32 @@
+const field = document.querySelector('.field'),
+    button = document.querySelector('.add'),
+    list = document.querySelector('.list');
+
+function createTask(value){
+    const task = document.createElement("div");
+    const chb = document.createElement("checkbox");
+    task.classList.add("task");
+    task.classList.add("success")
+    task.textContent = value;
+    chb.classList.add("status");
+    task.append(chb);
+    chb.addEventListener('click', completeTask);
+    return task;
+}
+
+function addTask(){
+    if (field.value != false){
+        const newTask = createTask(field.value);
+        list.appendChild(newTask);
+        field.value = null;
+        console.log(list);
+    } else{
+        alert("Строка пуста")
+    }
+}
+
+function completeTask(event){
+    const target = event.target;
+    node.parentElement.success.color = "green";
+}
+button.addEventListener('click', addTask);
